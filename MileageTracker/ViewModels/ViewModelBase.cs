5cdbdab257337637dc30d05e9bb11b2bae8374d6
Ari Cummings <ari@aricummings.com>
2025-08-15 20:19:24 -0400

    init

---
+++ MileageTracker/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MileageTracker.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ MileageTracker/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace MileageTracker.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
